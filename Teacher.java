68b21d6d31d0ac3005098559854e807a4ddf47dd
jgant014 <102830052+jgant014@users.noreply.github.com>
2022-04-09 15:20:52 -0400

    Add files via upload

--- Teacher.java
+++ Teacher.java
@@ -2,77 +2,99 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Teacher {
-    private String userName ;
-    private String password ;
-    private String name ;
-    private ArrayList<Courses> listCourses= new ArrayList<>() ;
-    public Teacher(String userName , String password , String name) {
-        this.userName = userName ;
-        this.password = password ;
-        this.name = name ;
+    private String userName;
+    private String password;
+    private String name;
+    private ArrayList<Courses> listCourses = new ArrayList<>();
+
+    //Constructor for existing teacher
+    //Constructs a teacher object with userName, password, name
+    public Teacher(String userName, String password, String name) {
+        this.userName = userName;
+        this.password = password;
+        this.name = name;
     }
-    public Teacher(String userName , String password , String name , String teacherListFileName) {
-        this.userName = userName ;
-        this.password = password ;
-        this.name = name ;
+
+    //Constructor for new teacher
+    //Constructs a teacher object with userName, password, name
+    //Adds teacher name, userName, and password to teacherListFile
+    public Teacher(String userName, String password, String name, String teacherListFileName) {
+        this.userName = userName;
+        this.password = password;
+        this.name = name;
         try {
-            File f = new File(teacherListFileName) ;
-            f.createNewFile() ;
-            BufferedReader br = new BufferedReader(new FileReader(f)) ;
-            String str = br.readLine() ;
-            String s = "" ;
-            while(str != null) {
-                s += str + "\n" ;
-                str = br.readLine() ;
+            File f = new File(teacherListFileName);
+            f.createNewFile();
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String str = br.readLine();
+            String s = "";
+            while (str != null) {
+                s += str + "\n";
+                str = br.readLine();
             }
+            br.close();
             s += name + "$$" + userName + "$$" + password + "\n";
-            PrintWriter pw = new PrintWriter(new FileWriter(f), true) ;
-            pw.print(s) ;
-            pw.close() ;
+            PrintWriter pw = new PrintWriter(new FileWriter(f), true);
+            pw.print(s);
+            pw.close();
         } catch (FileNotFoundException e) {
-            e.printStackTrace() ;
+            e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace() ;
+            e.printStackTrace();
         }
     }
 
+    //Getter for name
     public String getName() {
         return this.name;
     }
 
+    //Getter for password
     public String getPassword() {
         return this.password;
     }
 
+    //Getter for userName
     public String getUserName() {
         return this.userName;
     }
+
+    //Getter for ArrayList that stores all of the courses objects
     public ArrayList<Courses> getListCourses() {
-        return this.listCourses ;
+        return this.listCourses;
     }
-    public boolean createCourse(String courseName , String courseListFileName) throws IOException {
-        this.listCourses.add(new Courses(courseName , courseListFileName)) ;
-        return true ;
+
+    //Method that adds a course object to the courseList array
+    public boolean createCourse(String courseName, String courseListFileName) throws IOException {
+        this.listCourses.add(new Courses(courseName, courseListFileName));
+        return true;
     }
-    public boolean enterCourse(String courseName , String courseListFileName) throws IOException {
-        File f = new File(courseListFileName) ;
-        BufferedReader br = new BufferedReader(new FileReader(f)) ;
-        String er = br.readLine() ;
-        if (er == null) {
-            return false ;
+
+    //Checks if courseName is inside of the courseListFile
+    //Returns true if courseName is inside of the courseListFile
+    //Returns false if courseName is not inside of the courseListFile
+    public boolean enterCourse(String courseName, String courseListFileName) throws IOException {
+        File f = new File(courseListFileName);
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        String str = br.readLine();
+        if (str == null) {
+            br.close();
+            return false;
         } else {
-            int c = 0 ;
-            while(er != null) {
-                if(er.equals(courseName)) {
-                    c++ ;
+            int c = 0;
+            while (str != null) {
+                if (str.equals(courseName)) {
+                    c++;
                 }
-                er = br.readLine() ;
+                str = br.readLine();
             }
             if (c == 0) {
-                return false ;
+                br.close();
+                return false;
             } else {
-                this.listCourses.add(new Courses(courseName)) ;
-                return true ;
+                br.close();
+                this.listCourses.add(new Courses(courseName));
+                return true;
             }
         }
     }
