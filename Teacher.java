720f629d8a6e7ea082d239aeb1fd49c06ed82597
ritwikjain17 <98117842+ritwikjain17@users.noreply.github.com>
2022-04-11 05:11:03 -0400

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -18,7 +18,8 @@ public class Teacher {
     //Constructor for new teacher
     //Constructs a teacher object with userName, password, name
     //Adds teacher name, userName, and password to teacherListFile
-    public Teacher(String userName, String password, String name, String teacherListFileName) {
+    public Teacher(String userName, String password,
+                   String name, String teacherListFileName) {
         this.userName = userName;
         this.password = password;
         this.name = name;
@@ -32,7 +33,6 @@ public class Teacher {
                 s += str + "\n";
                 str = br.readLine();
             }
-            br.close();
             s += name + "$$" + userName + "$$" + password + "\n";
             PrintWriter pw = new PrintWriter(new FileWriter(f), true);
             pw.print(s);
@@ -44,28 +44,33 @@ public class Teacher {
         }
     }
 
-    //Getter for name
     public String getName() {
         return this.name;
     }
 
-    //Getter for password
     public String getPassword() {
         return this.password;
     }
 
-    //Getter for userName
     public String getUserName() {
         return this.userName;
     }
 
-    //Getter for ArrayList that stores all of the courses objects
     public ArrayList<Courses> getListCourses() {
         return this.listCourses;
     }
 
     //Method that adds a course object to the courseList array
-    public boolean createCourse(String courseName, String courseListFileName) throws IOException {
+    public boolean createCourse(String courseName, String courseListFileName)
+            throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(courseListFileName));
+        String str = br.readLine();
+        while (str != null) {
+            if (str.equals(courseName)) {
+                return false;
+            }
+            str = br.readLine();
+        }
         this.listCourses.add(new Courses(courseName, courseListFileName));
         return true;
     }
@@ -73,26 +78,24 @@ public class Teacher {
     //Checks if courseName is inside of the courseListFile
     //Returns true if courseName is inside of the courseListFile
     //Returns false if courseName is not inside of the courseListFile
-    public boolean enterCourse(String courseName, String courseListFileName) throws IOException {
+    public boolean enterCourse(String courseName, String courseListFileName)
+            throws IOException {
         File f = new File(courseListFileName);
         BufferedReader br = new BufferedReader(new FileReader(f));
-        String str = br.readLine();
-        if (str == null || str.equals("") || str.equals(" ")) {
-            br.close();
+        String er = br.readLine();
+        if (er == null || er.equals("") || er.equals(" ")) {
             return false;
         } else {
             int c = 0;
-            while (str != null) {
-                if (str.equals(courseName)) {
+            while (er != null) {
+                if (er.equals(courseName)) {
                     c++;
                 }
-                str = br.readLine();
+                er = br.readLine();
             }
             if (c == 0) {
-                br.close();
                 return false;
             } else {
-                br.close();
                 this.listCourses.add(new Courses(courseName));
                 return true;
             }
